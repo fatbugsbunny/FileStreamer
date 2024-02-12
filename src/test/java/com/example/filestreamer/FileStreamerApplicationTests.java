b559c242371e66916ec1d1f47df04d5c00a56477
fatbugsbunny <verses.v20@gmail.com>
2024-02-12 17:19:38 +0100

    Commit

---
+++ src/test/java/com/example/filestreamer/FileStreamerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filestreamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStreamerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
